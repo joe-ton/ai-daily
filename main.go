25b2ce1b0b28723010ffccbf1c231dc2ec642f0f
joe-ton <joeton.public@gmail.com>
2024-04-10 07:23:12 -0700

    LC: Practice reverseString

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/joe-ton/goLC/practice"
+)
+
+func main() {
+	str := "abc"
+	result := practice.ReverseString(str)
+	fmt.Println("Result:", result)
+}
