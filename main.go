3ad8269796a151c3a9ebc2505cac760ba15aca3d
joe-ton <joeton.public@gmail.com>
2024-07-30 20:28:14 -0700

    Test

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+func TwoSum(nums []int, target int) []int {
+	hashMap := make(map[int]int)
+
+	for idx, num := range nums { // Corrected loop variable names
+		complement := target - num
+		if index, ok := hashMap[complement]; ok {
+			return []int{index, idx} // Corrected the order of indices in the return statement
+		}
+		hashMap[num] = idx
+	}
+	return []int{}
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	res := TwoSum(nums, target)
+	fmt.Println(res) // Output: [0, 1]
 }
