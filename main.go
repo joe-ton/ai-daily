ea5327000003de1d92aac001ceecf39940c33dab
joe-ton <joeton.public@gmail.com>
2024-08-29 23:31:53 -0700

    Test...

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// shorthand
+	name := "Joe"
+
+	// longhand
+	var name2 string
+	name2 = "Bella"
+
+	fmt.Println("Name1:", name)
+	fmt.Println("Name2:", name2)
+}
