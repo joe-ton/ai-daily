7424f55fa1fbb7440c9e754f21a97716f6ed77b0
joe-ton <joeton.public@gmail.com>
2025-03-19 17:26:39 -0700

    goLC practice: twoSum

--- main.go
+++ main.go
@@ -1,43 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+/*
+Find two integers from slice to add to sum of target.
+
+Parameters:
+- 
+*/
+func twoSum(integers []int, target int) []int {
+	numMap := make(map[int]int)
+	for i, num := range integers {
+		complement := target - num
+		indexCompl, found := numMap[complement]; found {
+			[]int{num, indexCompl}
+		}
+	}
+}
 
 func main() {
 	integers := []int{2, 7, 11, 15}
-	target := 9
-	result := getTwoSum(target, integers)
-	fmt.Println("Indices:", result)
+	result := twoSum(integers, 9)
+	fmt.Println(result)
 }
-
-/*
-Find two integers from slice to add up to targeted sum.
-
-Parameters:
-- target int: total sum needed to add up to
-- integers []int: integers given
-
-Returns:
-- []int: empty or 2 slices of indices whose values add to targeted sum
-
-Tradeoff Complexities:
-1. Brute Force: Time O(n^2), Space O(1)
--> 2. HashMap: Time O(n), Space O(n)
-3. Sorted / 2 pointer: Time O(nlogn), Space O(n) or O(1)
-*/
-func getTwoSum(target int, integers []int) []int {
-	numMap := make(map[int]int) // complement counter
-
-	for i, num in range integers {
-		complement := target - num
-		// found { } is called a comma-ok idiom, generates a boolean
-		if compIndex, found := numMap[complement]; found {
-			return []int{compIndex, i}
-		}
-		numMap[num] = i
-	}
-	return []int{}
-}
-
-
-
-
