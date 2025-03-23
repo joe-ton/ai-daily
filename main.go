87fdfaf00e061bc175155a0e6ca9e4dd677e2278
joe-ton <joeton.public@gmail.com>
2025-03-23 08:37:06 -0700

    practice neovim

--- main.go
+++ main.go
@@ -1,39 +1,12 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+)
 
 func main() {
-    integers := []int{1, 2, 3, 4}
-    target := 5
-    result := twoSum(integers, target)
-    fmt.Println("Result:", result)
-}
-
-/*
-Find two values from given slice adding to target sum.
-
-Parameters:
-- integers []int: given integers
-- target int: target sum
-
-Returns:
-- []int: indices
-
-Solutions:
-1. Brute Force: Time (n^2), Space (1)
-2. Map: Time (n), Space (n)
-3. Sorted: Time (nlogn), Space(n)
-*/
-func twoSum(integers []int, target int) []int {
-    numMap := make(map[int]int)
-
-    for i, num := range integers {
-        complement := target - num
-        if complIdx, found := numMap[complement]; found {
-            return []int{i, complIdx}
-        }
-        numMap[num] = i
-    }
-
-    return []int{}
+    resp, err := http.Get("http://www.google.com")
+    fmt.Println("Resp:", resp)
+    fmt.Println("Err:", err)
 }
