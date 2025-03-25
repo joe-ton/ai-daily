78a5f9bcf6212d9cf6f470314bc2d5f0a19493f7
joe-ton <joeton.public@gmail.com>
2025-03-25 08:08:08 -0700

    Trying go mod init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/joe-ton/goLC
+
+go 1.24.1
--- main.go
+++ main.go
@@ -1,17 +1,37 @@
+
 package main
 
 import (
     "fmt"
 )
 
-type twoSumData struct {
+type TwoSumInput struct {
     Integers []int `json:"integers"`
     Target int `json:"target"`
 }
 
 func main() {
-    input := twoSumData{
+    input := TwoSumInput {
         Integers: []int{1, 2, 3, 4},
-        Target: 9,
+        Target: 7,
+    }
+    i, j, ok := twoSum(input)
+    if ok {
+        fmt.Printf("Indices: %d and %d\n", i, j)
+    } else {
+        fmt.Println("Invalid Indices")
     }
 }
+
+func twoSum(input TwoSumInput) (int, int, bool) {
+    compMap := make(map[int]int)
+
+    for i, num := range input.Integers {
+        complement := input.Target - num
+        if compIndex, ok := compMap[complement]; ok {
+            return compIndex, i, true
+        }
+        compMap[num] = i
+    }
+    return 0, 0, false
+} 
