64ece9c1cee7e068529ae3a75135b7117579fc2c
joe-ton <joeton.public@gmail.com>
2025-03-28 17:42:40 -0700

    main.go

--- main.go
+++ main.go
@@ -15,25 +15,21 @@ func (m MapSolver) TwoSum(nums []int, target int) ([]int, error) {
     seen := make(map[int]int)
 
     for i, num := range nums {
-        complement := target - num
-
+        complement := num - i
         if compIdx, found := seen[complement]; found {
             return []int{compIdx, i}, nil
         }
         seen[num] = i
     }
-    return nil, errors.New("Wrong Indices")
+    return nil, errors.New("Invalid")
 }
 
 func main() {
     solver := MapSolver{}
-
     resp, err := solver.TwoSum([]int{1, 2, 3, 4}, 7)
     if err != nil {
-        fmt.Println("Error:", err.Error())
+        fmt.Println("Error:", err)
     } else {
-        fmt.Println("Indices:", resp)
+        fmt.Println("Resp:", resp)
     }
-
 }
-
