dcbe87374cb662dbd6987f60f600c4372ce2d858
joe-ton <joeton.public@gmail.com>
2025-03-30 19:14:18 -0700

    Added trying others

--- main.go
+++ main.go
@@ -1,30 +1,22 @@
-
 package main
 
 import (
-    "fmt"
     "errors"
+    "fmt"
 )
 
-func TwoSum(nums []int, target int) ([]int, error) {
+type TwoSumSolver interface {
+    TwoSum(nums []int, target int) ([]int, int)
+}
+
+type MapSolver struct {}
+
+func TwoSum(nums []int, target int) ([]int, int) {
     seen := make(map[int]int)
 
     for i, num := range nums {
         complement := target - num
 
-        if compIdx, found := seen[complement]; found {
-            return []int{compIdx, i}, nil
-        }
-        seen[num] = i
-    }
-    return nil, errors.New("Invalid Indices")
-}
-
-func main() {
-    resp, err := TwoSum([]int{1, 2, 3, 4}, 7)
-    if err != nil {
-        fmt.Println("Errors:", err.Error())
-    } else {
-        fmt.Println("Response:", resp)
+        if 
     }
 }
