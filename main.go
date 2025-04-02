b8878f1bf13b0be0b77134e514cb04c0f20237fa
joe-ton <joeton.public@gmail.com>
2025-04-02 09:48:31 -0700

    goLC: 2sum

--- main.go
+++ main.go
@@ -5,20 +5,36 @@ import (
     "errors"
 )
 
-type TwoSumer interface {
+type MapSolver struct {}
+
+type TwoSumInterface interface {
     TwoSum(nums []int, target int) ([]int, error)
 }
-
-type MapSolver struct {}
 
 func (m MapSolver) TwoSum(nums []int, target int) ([]int, error) {
     seen := make(map[int]int)
 
     for i, num := range nums {
         complement := target - num
-        
-        if ta
+
+        if compIdx, found := seen[complement]; found {
+            return []int{compIdx, i}, nil
+        }
+        seen[num] = i
     }
+    return nil, errors.New("Invalid Number")
 }
 
+func main() {
+    solver := TwoSumInterface(MapSolver{})
+    nums := []int{1, 2, 3, 4}
+    target := 7
 
+    resp, err := solver.TwoSum(nums, target)
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+
+    fmt.Println("Response:", resp)
+}
