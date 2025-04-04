3f849cefc6379d25ad95740fa8187cd41d7953d2
Joe Ton <joe@tonindustries.com>
2025-04-04 09:32:53 -0700

    practice: cpp lc1

---
+++ cpp/two_sum/main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <utility> 
+#include <unordered_map>
+#include <span>
+
+using namespace std;
+
+pair<int, int> twoSum(span<int> nums, int target) {
+    unordered_map<int, int> seen;
+
+    for (int i = 0; i < nums.size(); ++i) {
+
+    }
+}
