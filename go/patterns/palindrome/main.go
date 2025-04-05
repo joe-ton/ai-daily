ad80e43a771c0c7153070ac670b99c9fa62c4275
Joe Ton <joe@tonindustries.com>
2025-04-04 23:15:10 -0700

    practice: go palindrome

--- go/patterns/palindrome/main.go
+++ go/patterns/palindrome/main.go
@@ -2,13 +2,5 @@ package main
 
 import (
     "fmt"
-    "errors"
-    "unicode"
-    "string"
 )
 
-func isPalindrome(s string) bool {
-    // in go, strings are sequences of bytes
-    var []string rune
-
-}
