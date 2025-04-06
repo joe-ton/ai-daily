6273eaa3ea2deb1fa4ffec00c0066cae9c094cab
Joe Ton <joe@tonindustries.com>
2025-04-06 14:17:03 -0700

    practice: go interfaces

--- go/patterns/easy/twosum/main.go
+++ go/patterns/easy/twosum/main.go
@@ -5,63 +5,34 @@ import (
     "errors"
 )
 
-type TwoSumSolver interface {
-    Run() ([]int, error)
-}
-
 type TwoSumData struct {
     Nums []int
     Target int
 }
 
-func NewTwoSumData(nums []int, target int) *TwoSumData {
-    return &TwoSumData{
-        Nums: nums,
-        Target: target,
-    }
-}
-
-func (d TwoSumData) TwoSumBrute() ([]int, error) {
-    if len(d.Nums) < 2 {
-        return nil, errors.New("Invalid count")
-    }
-
-    for i := 0; i < len(d.Nums); i++ {
-        for j := 0; j < len(d.Nums)-1; j++ {
-            return []int{d.Nums[i], d.Nums[j]}, nil
-        }
-    }
-    return nil, errors.New("Invalid Indices")
-}
-
-func (d TwoSumData) TwoSumHash() ([]int, error) {
-    if len(d.Nums) < 2 {
+func TwoSum(data TwoSumData) ([]int, error) {
+    if len(data.Nums) < 2 {
         return nil, errors.New("Invalid count")
     }
     seen := make(map[int]int)
 
-    for idx, num := range d.Nums {
-        complement := d.Target - num
+    for idx, num := range data.Nums {
+        complement := data.Target - num
         if compIdx, found := seen[complement]; found {
-            return []int{compIdx, idx}, nil
-        } 
-        seen[num] = idx
+            return []int{compIdx, idx}, nil // early return
+        }
+        seen[num] = idx // tracker
     }
-
-    return nil, errors.New("Invalid Indices")
+    return nil, errors.New("Invalid indices") // fallback
 }
 
-func (d *TwoSumData) Run() ([]int, error) {
-    return d.TwoSumHash()
-}
-
-func main()  {
+func main() {
     data := TwoSumData{Nums: []int{1, 2, 3, 4}, Target: 7}
-    resp, err := data.Run()
-
+    resp, err := TwoSum(data)
     if err != nil {
         fmt.Println("Error:", err.Error())
     } else {
         fmt.Println("Response:", resp)
     }
 }
+
