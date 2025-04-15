31e0a2f7c93345b6ecdcb57ed8c76396eca04996
Joe Ton <joe@tonindustries.com>
2025-04-14 21:53:50 -0700

    projects: todo

---
+++ go/patterns/shutdwon2/cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+}
