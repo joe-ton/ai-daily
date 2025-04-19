f9c48305f3051341444b2710e26510a79265a091
Joe Ton <joe@tonindustries.com>
2025-04-19 01:21:17 -0700

    practice: go logger

---
+++ go/patterns/easy/twoSum/cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+}
