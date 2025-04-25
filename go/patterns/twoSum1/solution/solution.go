fc98ec44a086a02850d2eccbe859470a8b4c5b1e
Joe Ton <joe@tonindustries.com>
2025-04-25 16:47:27 -0700

    go: logging

--- go/patterns/twoSum1/solution/solution.go
+++ go/patterns/twoSum1/solution/solution.go
@@ -7,18 +7,23 @@ type TwoSum struct {
 	Target int
 }
 
+// Find indices based off of the nums whose values equal target
 func (t TwoSum) Find() (indices []int, err error) {
 	if len(t.Nums) < 2 {
 		return nil, errors.New("Invalid count of nums")
 	}
+	// TODO: empty inputs
 
+	// target = var1 + var2
 	seen := make(map[int]int)
 
 	for idx, num := range t.Nums {
 		complement := t.Target - num
 		if compIdx, found := seen[complement]; found {
-			return []int{compIdx, idx}, nil
+			return []int{compIdx, idx}, nil // early exist
 		}
+		seen[num] = idx // update to tracker
 	}
-	return nil, errors.New("Invalid count of nums")
+	// fallback exist
+	return nil, errors.New("Invalid indices")
 }
