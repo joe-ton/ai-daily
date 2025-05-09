aa97d7e47f3d4052e4c37b624f211d225fee3b6d
Joe Ton <joe@tonindustries.com>
2025-05-09 00:25:49 -0700

    project: go

--- go/patterns/tmp1/cmd/main.go
+++ go/patterns/tmp1/cmd/main.go
@@ -1,19 +1,21 @@
 package main
 
 type MovingAverage struct {
-	size  int   // maximum window size
-	queue []int // slice to store the last N integers
-	sum   int   // total current amount based on queue
+	size  int   // max window size, fixed
+	queue []int // slice of last N integers
+	sum   int   // total value of current N integers
 }
 
 func Constructor(size int) MovingAverage {
 	return MovingAverage{
 		size:  size,
 		queue: make([]int, 0, size),
-		sum:   0,
+		sum:   size,
 	}
 }
 
 func (m *MovingAverage) Next(val int) float64 {
-	m.queue = append(m.queue, val)
+	m.queue = append()
 }
+
+327419653001
