59be01f4f6bc4daaab4b0047b3abe3f147bcf30b
Joe Ton <joe@tonindustries.com>
2025-05-16 04:33:05 -0700

    practice: go logging

---
+++ go/pattern/tmp2/cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// bootstrap
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	ctx, done := signal.NotifyContext(
+		context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer done()
+
+	if err := run(ctx, logger); err != nil {
+		logger.Error("Application run failed", "run", err)
+	}
+}
+
+// application
+func run(ctx context.Context, logger *slog.Logger) error {
+	<-ctx.Done()
+	return nil
+}
---
+++ go/pattern/tmp2/service/user/routes.go
@@ -0,0 +1,14 @@
+package user
+
+import "github.com/gorilla/mux"
+
+type Handler struct {
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+
+}
