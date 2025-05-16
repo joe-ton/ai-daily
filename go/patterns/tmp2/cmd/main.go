3f027f73ceb0c105e5da3ea933c472a359662cc7
Joe Ton <joe@tonindustries.com>
2025-05-16 01:05:12 -0700

    practice: go logging

---
+++ go/patterns/tmp2/cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/joe-ton/cmd/api"
+)
+
+// bootstrap
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	ctx, done := signal.NotifyContext(
+		context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer done()
+
+	if err := run(ctx, logger); err != nil {
+		logger.Error("Application run failed", "run", err)
+		os.Exit(1)
+	}
+}
+
+// application
+func run(ctx context.Context, logger *slog.Logger) error {
+	server := api.NewAPIServer(":8080", nil)
+	if err := server.Run(); err != nil {
+		logger.Error("Application server port failed", "run", err)
+		os.Exit(1) // fatal
+	}
+
+	<-ctx.Done()
+	return nil
+}
