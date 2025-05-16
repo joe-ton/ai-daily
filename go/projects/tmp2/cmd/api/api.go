3c5cfe7a7f2e41d8c4909b86e192500e67e472d7
Joe Ton <joe@tonindustries.com>
2025-05-15 22:37:46 -0700

    projects: go

---
+++ go/projects/tmp2/cmd/api/api.go
@@ -0,0 +1,19 @@
+package api
+
+import "database/sql"
+
+type APIServer struct {
+	addr string // port server
+	db   *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *APIServer) Run() error {
+
+}
