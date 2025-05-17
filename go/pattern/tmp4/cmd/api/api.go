16d902010c67a96f51e2799f74d8bb0a0f030afa
Joe Ton <joe@tonindustries.com>
2025-05-16 19:24:38 -0700

    practice: go

---
+++ go/pattern/tmp4/cmd/api/api.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"database/sql"
+	"log/slog"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	addr string // port
+	db   *sql.DB
+	log  *slog.Logger
+}
+
+func NewAPIServer(addr string, db *sql.DB, log *slog.Logger) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+		log:  log,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	s.log.Info("Router setup", "router", subrouter)
+
+	return http.ListenAndServe(s.addr, nil)
+}
---
+++ go/pattern/tmp4/cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/joe-ton/cmd/api"
+)
+
+// bootstrap
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	ctx, done := signal.NotifyContext(
+		context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer done()
+
+	if err := run(ctx, logger); err != nil {
+		logger.Error("Application failed run", "run", err)
+	}
+}
+
+// application
+func run(ctx context.Context, logger *slog.Logger) error {
+	server := api.NewAPIServer(":8080", nil, logger)
+	if err := server.Run(); err != nil {
+		logger.Error("Server setup failed", "server", err)
+	}
+
+	<-ctx.Done()
+	return nil
+}
