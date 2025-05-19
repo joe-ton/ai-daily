f79655576b20f5d1481a4d67a41b4d0cfd9c32d5
Joe Ton <joe@tonindustries.com>
2025-05-19 10:12:10 -0700

    projects: reset

--- go/pattern/tmp3/go.mod
+++
@@ -1,9 +0,0 @@
-module github.com/joe-ton
-
-go 1.24.2
-
-require (
-	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/go-sql-driver/mysql v1.9.2 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
-)
---
+++ go/patterns/tmp1/db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"database/sql"
+	"log/slog"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+type MySQLStorage struct {
+	cfg mysql.Config
+	log *slog.Logger
+}
+
+func NewMySQLStorage(cfg mysql.Config, log *slog.Logger) *MySQLStorage {
+	return &MySQLStorage{
+		cfg: cfg,
+		log: log,
+	}
+}
+
+func (s *MySQLStorage) Build() (*sql.DB, error) {
+	db, err := sql.Open("mysql", s.cfg.FormatDSN())
+	if err != nil {
+		s.log.Error("Storage fatal", "sql", err)
+		os.Exit(1)
+	}
+	return db, nil
+}
