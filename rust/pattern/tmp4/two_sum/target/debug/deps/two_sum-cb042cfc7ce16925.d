89c5b770b663175edb0fe730a309d1010dc21654
Joe Ton <joe@tonindustries.com>
2025-05-21 16:48:34 -0700

    practice: go

---
+++ rust/pattern/tmp4/two_sum/target/debug/deps/two_sum-cb042cfc7ce16925.d
@@ -0,0 +1,5 @@
+/home/joe/projects/ai-daily/rust/pattern/tmp4/two_sum/target/debug/deps/two_sum-cb042cfc7ce16925: src/main.rs
+
+/home/joe/projects/ai-daily/rust/pattern/tmp4/two_sum/target/debug/deps/two_sum-cb042cfc7ce16925.d: src/main.rs
+
+src/main.rs:
