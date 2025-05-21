3a8db4cb48f99c0506235b03337a8ba6efdb7500
Joe Ton <joe@tonindustries.com>
2025-05-20 17:44:00 -0700

    pracice: docker setup

---
+++ go/patterns/tmp5/solution/solution_test.go
@@ -0,0 +1,68 @@
+package solution
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func TestTwoSumBrute(t *testing.T) {
+	tests := []struct {
+		name    string
+		nums    []int
+		target  int
+		want    []int // indices
+		wantErr error
+	}{
+		{
+			name:    "Test complete",
+			nums:    []int{1, 2, 3, 4},
+			target:  7,
+			want:    []int{2, 3}, // indices
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			twoSum := TwoSum{Nums: tt.nums, Target: tt.target}
+			got, err := twoSum.FindBrute()
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("Got %v, Want %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Got %v, Want %v", got, tt.want)
+			}
+		})
+	}
+}
+func TestTwoSumOptimize(t *testing.T) {
+	tests := []struct {
+		name    string
+		nums    []int
+		target  int
+		want    []int // indices
+		wantErr error
+	}{
+		{
+			name:    "Test complete",
+			nums:    []int{1, 2, 3, 4},
+			target:  7,
+			want:    []int{2, 3}, // indices
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			twoSum := TwoSum{Nums: tt.nums, Target: tt.target}
+			got, err := twoSum.FindOptimize()
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("Got %v, Want %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Got %v, Want %v", got, tt.want)
+			}
+		})
+	}
+}
