7c14fac355ed0cec441fa11ad99aa2bd9dc0ff43
Joe Ton <joe@tonindustries.com>
2025-05-21 07:34:28 -0700

    practice: rust

---
+++ rust/patterns/tmp1/hello_world/target/debug/deps/hello_world-83c3dbb683f44516.d
@@ -0,0 +1,5 @@
+/home/joe/projects/ai-daily/rust/patterns/tmp1/hello_world/target/debug/deps/hello_world-83c3dbb683f44516: src/main.rs
+
+/home/joe/projects/ai-daily/rust/patterns/tmp1/hello_world/target/debug/deps/hello_world-83c3dbb683f44516.d: src/main.rs
+
+src/main.rs:
---
+++ rust/patterns/tmp3/greeter/target/debug/greeter.d
@@ -0,0 +1,1 @@
+/home/joe/projects/ai-daily/rust/patterns/tmp3/greeter/target/debug/greeter: /home/joe/projects/ai-daily/rust/patterns/tmp3/greeter/src/main.rs
