85e3eece63d867327fa6c9f5479b4b231effb033
Joe Ton <joe@tonindustries.com>
2025-05-22 02:54:19 -0700

    practice: rust

---
+++ rust/patterns/tmp1/tmp1/target/debug/deps/tmp1-64e8a010d0a4a461.d
@@ -0,0 +1,5 @@
+/home/joe/projects/ai-daily/rust/patterns/tmp1/tmp1/target/debug/deps/tmp1-64e8a010d0a4a461: src/main.rs
+
+/home/joe/projects/ai-daily/rust/patterns/tmp1/tmp1/target/debug/deps/tmp1-64e8a010d0a4a461.d: src/main.rs
+
+src/main.rs:
---
+++ rust/patterns/tmp1/tmp1/target/debug/tmp1.d
@@ -0,0 +1,1 @@
+/home/joe/projects/ai-daily/rust/patterns/tmp1/tmp1/target/debug/tmp1: /home/joe/projects/ai-daily/rust/patterns/tmp1/tmp1/src/main.rs
