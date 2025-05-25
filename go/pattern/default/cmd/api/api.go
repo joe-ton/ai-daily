aad4b7c3625b9ad4ecc291a2283668f123435226
Joe Ton <joe@tonindustries.com>
2025-05-25 13:12:26 -0700

    practice: go

--- go/pattern/default/cmd/api/api.go
+++ go/pattern/default/cmd/api/api.go
@@ -3,10 +3,32 @@ package api
 import (
 	"database/sql"
 	"log/slog"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Server struct {
 	addr string
 	db   *sql.DB
 	log  *slog.Logger
+}
+
+func NewServer(addr string, db *sql.DB, log *slog.Logger) *Server {
+	return &Server{
+		addr: addr,
+		db:   db,
+		log:  log,
+	}
+}
+
+// Combine router + handler
+func (s *Server) Run() error {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoute(subrouter)
+
+	return http.ListenAndServe(s.addr, nil)
 }
