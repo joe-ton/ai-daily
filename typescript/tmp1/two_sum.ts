2101caddaaf8959f79fd5059ce49f568ab7cfa2e
Joe Ton <joe@tonindustries.com>
2025-05-27 13:49:12 -0700

    practice: typescript

---
+++ typescript/tmp1/two_sum.ts
@@ -0,0 +1,14 @@
+function run(nums: number[], target: number): number[] {
+    const seen = new Map<number, number>();
+}
+
+
+
+function main() {
+    const nums = [1, 2, 3, 4];
+    const target = 7;
+    const resp = run(nums, target);
+    console.log(result);
+}
+
+main();
