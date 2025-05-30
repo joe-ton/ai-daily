2205240ee2c09ced54e646549d37f7f54d6f35e7
Joe Ton <joe@tonindustries.com>
2025-05-30 07:59:39 -0700

    pratice: rust and python

---
+++ rust/patterns/tmp2/hello/target/debug/deps/hello-9b04350e5d00f92d.d
@@ -0,0 +1,5 @@
+/home/joe/projects/ai-daily/rust/patterns/tmp2/hello/target/debug/deps/hello-9b04350e5d00f92d: src/main.rs
+
+/home/joe/projects/ai-daily/rust/patterns/tmp2/hello/target/debug/deps/hello-9b04350e5d00f92d.d: src/main.rs
+
+src/main.rs:
---
+++ rust/patterns/tmp2/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/joe/projects/ai-daily/rust/patterns/tmp2/hello/target/debug/hello: /home/joe/projects/ai-daily/rust/patterns/tmp2/hello/src/main.rs
