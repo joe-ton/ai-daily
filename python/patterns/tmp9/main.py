869f72fbfef0b844aae4e8837e3b2f6e4096b92c
Joe Ton <joe@tonindustries.com>
2025-05-31 21:59:41 -0700

    practice python

---
+++ python/patterns/tmp9/main.py
@@ -0,0 +1,21 @@
+
+def main():
+    nums = [1, 2, 3, 4]
+    target = 7
+    resp = run(nums, target)
+    print(resp)
+
+def run(nums: list, target: int) -> tuple:
+    seen = {}
+
+    for idx, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            comp_idx = seen[complement]
+            return (comp_idx, idx)
+        seen[num] = idx
+
+    return ()
+
+if __name__ == "__main__":
+    main()
