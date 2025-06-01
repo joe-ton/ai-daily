063a32e29f5dd9fa8ec625de0b3f4a31a83b7a76
Joe Ton <joe@tonindustries.com>
2025-06-01 09:35:25 -0700

    practice: typescript

---
+++ typescript/tmp8/main.ts
@@ -0,0 +1,17 @@
+
+function main() {
+    const nums = [1, 2, 3, 4];
+    const target = 7;
+    let resp = run(nums, target);
+    console.log(resp);
+}
+
+function run(nums: number[], target: number): [number, number] {
+    const seen = new Map<number, number>();
+
+    for (let i = 0; i < nums.length; i++) {
+
+    }
+}
+
+main();
