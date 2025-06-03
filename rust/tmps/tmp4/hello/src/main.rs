04e71abda9d50c05d146a42e0ac5a6b1145f7f07
Joe Ton <joe@tonindustries.com>
2025-06-02 22:01:11 -0700

    practice: rust

---
+++ rust/tmps/tmp4/hello/src/main.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+fn main() {
+    let nums = vec![1, 2, 3, 4];
+    let target = 7;
+    let resp = two_sum(&nums, target);
+
+    println!("Response: {:?}", resp);
+}
+
+pub fn two_sum(nums: &[i32], target: i32) -> Vec<usize> {
+    let mut seen = HashMap::new();
+
+    for (idx, num) in nums.iter().enumerate() {
+        let complement = target - num;
+        if let Some(&comp_idx) = seen.get(&complement) {
+            return vec![comp_idx, idx];
+        }
+        seen.insert(num, idx);
+    }
+    vec![]
+}
