5b82b88f7f1b056ee37b63be306ba9eff821bbfe
Joe Ton <joe@tonindustries.com>
2025-06-04 08:07:41 -0700

    practice: rust

--- rust/tmps/tmp4/hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-/home/joe/projects/ai-daily/rust/tmps/tmp4/hello/target/debug/hello: /home/joe/projects/ai-daily/rust/tmps/tmp4/hello/src/main.rs
--- rust/tmps/tmp6/hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-/home/joe/projects/ai-daily/rust/tmps/tmp6/hello/target/debug/hello: /home/joe/projects/ai-daily/rust/tmps/tmp6/hello/src/main.rs
