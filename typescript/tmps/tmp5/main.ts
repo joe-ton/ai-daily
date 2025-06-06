91af602c65ea812ba6e787ab11cab971a8763ede
Joe Ton <joe@tonindustries.com>
2025-06-06 09:33:56 -0700

    practice: typescript

---
+++ typescript/tmps/tmp5/main.ts
@@ -0,0 +1,28 @@
+
+function main() {
+    const nums = [1, 2, 3, 4];
+    const target = 7;
+    let resp = run(nums, target)
+    console.log("Response: ", resp)
+}
+
+function run(nums: number[], target: number): [number, number] | null {
+    const seen = new Map<number, number>();
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const complement = target - num;
+
+        if (seen.has(complement)) {
+            const compIdx = seen.get(complement);
+            return [compIdx, i];
+        }
+
+        seen.set(num, i);
+    }
+
+    return null
+}
+
+
+main();
