a880d8e9b1753ecdc93ccecedfd0f75c2d4c3216
Joe Ton <joe@tonindustries.com>
2025-06-07 10:32:52 -0700

    practice: go, rust

---
+++ go/tmps/tmp8/solution/solution.go
@@ -0,0 +1,26 @@
+package solution
+
+import "errors"
+
+type TwoSum struct {
+	Nums   []int
+	Target int
+}
+
+func (t TwoSum) Run() ([]int, error) {
+	if len(t.Nums) < 2 {
+		return nil, errors.New("Invalid count")
+	}
+
+	seen := make(map[int]int) // int to num
+
+	for idx, num := range t.Nums {
+		complement := t.Target - num
+		if compIdx, found := seen[complement]; found {
+			return []int{compIdx, idx}, nil
+		}
+		seen[num] = idx
+	}
+
+	return nil, errors.New("Invalid indices")
+}
---
+++ rust/tmpss/tmp4/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/joe/projects/ai-daily/rust/tmpss/tmp4/hello/target/debug/hello: /home/joe/projects/ai-daily/rust/tmpss/tmp4/hello/src/main.rs
