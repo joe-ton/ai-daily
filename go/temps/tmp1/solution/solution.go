d72b27d0be4f8492ee842c5014e8d7993048dfbb
Joe Ton <joe@tonindustries.com>
2025-06-09 17:59:52 -0700

    practice: go

--- go/temps/tmp1/solution/solution.go
+++ go/temps/tmp1/solution/solution.go
@@ -2,20 +2,18 @@ package solution
 
 import "errors"
 
-// Determine if given values have duplicate integers
-// Return if true
+// Determine if given numbers have duplicate values
+// If true, return true. Otherwise false
 func Is_duplicate(nums []int) (bool, error) {
-	// hash-set; tracker of previous values
+	// hash-set
 	seen := make(map[int]struct{}, len(nums))
 
-	// iterate, values
+	// iterate through
 	for _, val := range nums {
-		// find if current val in hashset
 		if _, dup := seen[val]; dup {
 			return true, nil
 		}
-		// remember value
 		seen[val] = struct{}{}
 	}
-	return false, errors.New("Invalid result")
+	return false, errors.New("Invalid bool")
 }
