37dcf0092947e957cf4cf9e0592231cd607862a7
Joe Ton <joe@tonindustries.com>
2025-06-12 23:28:05 -0700

    practice: go

---
+++ go/temps/tmp5/solution/solution.go
@@ -0,0 +1,25 @@
+package solution
+
+import "errors"
+
+type TwoSum struct {
+	Nums   []int
+	Target int
+}
+
+func (t TwoSum) Find() ([]int, error) {
+	if len(t.Nums) < 2 {
+		return nil, errors.New("Invalid count of Nums")
+	}
+
+	seen := make(map[int]int)
+
+	for idx, num := range t.Nums {
+		complement := t.Target - num
+		if compIdx, found := seen[complement]; found {
+			return []int{compIdx, idx}, nil
+		}
+		seen[num] = idx
+	}
+	return nil, errors.New("Invalid indices")
+}
