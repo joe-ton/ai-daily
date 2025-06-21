05e82afdfb60eab3f81f6d8a697ec85d037ad49d
Joe Ton <joe@tonindustries.com>
2025-06-20 22:26:32 -0700

    practice: go

---
+++ go/tmps/tmp6/solution/solution.go
@@ -0,0 +1,27 @@
+package solution
+
+import "errors"
+
+type TwoSum struct {
+	Nums   []int
+	Target int
+}
+
+// Find two indices whose values are equal to given target
+func (t TwoSum) Find() ([]int, error) {
+	if len(t.Nums) < 2 {
+		return nil, errors.New("Invalid indices")
+	}
+
+	seen := make(map[int]int)
+
+	for idx, num := range t.Nums {
+		complement := t.Target - num
+		if compIdx, found := seen[complement]; found {
+			return []int{compIdx, idx}, nil
+		}
+		seen[num] = idx
+	}
+
+	return nil, errors.New("Invalid indices")
+}
