bbc79fcbc36a68ddadc73ee0d87af2ed108e05bd
Joe Ton <joe@tonindustries.com>
2025-07-06 13:32:13 -0700

    practice: rust

---
+++ rust/tmps/tmp3/hello/target/debug/deps/hello-3cb7b5b9679b33d6.d
@@ -0,0 +1,7 @@
+/home/joe/projects/ai-daily/rust/tmps/tmp3/hello/target/debug/deps/libhello-3cb7b5b9679b33d6.rmeta: src/lib.rs
+
+/home/joe/projects/ai-daily/rust/tmps/tmp3/hello/target/debug/deps/libhello-3cb7b5b9679b33d6.rlib: src/lib.rs
+
+/home/joe/projects/ai-daily/rust/tmps/tmp3/hello/target/debug/deps/hello-3cb7b5b9679b33d6.d: src/lib.rs
+
+src/lib.rs:
---
+++ rust/tmps/tmp3/hello/target/debug/deps/hello-5e3a9e806439159c.d
@@ -0,0 +1,6 @@
+/home/joe/projects/ai-daily/rust/tmps/tmp3/hello/target/debug/deps/hello-5e3a9e806439159c: src/main.rs src/main_test.rs
+
+/home/joe/projects/ai-daily/rust/tmps/tmp3/hello/target/debug/deps/hello-5e3a9e806439159c.d: src/main.rs src/main_test.rs
+
+src/main.rs:
+src/main_test.rs:
