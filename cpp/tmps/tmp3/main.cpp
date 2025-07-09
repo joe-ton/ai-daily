adbefe6d7c4a37a97a283f3102090382e59777b3
joe-ton <joeton.public@gmail.com>
2025-07-09 03:49:08 -0700

    practice: cpp

---
+++ cpp/tmps/tmp3/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+std::pair<int, int> run(const std::vector<int>& nums, int target) {
+    std::unordered_map<int, int> seen;
+
+    for (int i = 0; i < nums.size(); i++) {
+
+    }
+}
---
+++ cpp/tmps/tmp4/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+/*#include <vector>*/
+/*#include <unorderd_map>*/
+
+int main() {
+    int target;
+
+    std::cout << target << std::endl;
+}
