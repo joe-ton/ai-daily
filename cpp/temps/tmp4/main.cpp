7af55c76e1cb60912050381883ae2ac3191e7159
joe-ton <joeton.public@gmail.com>
2025-07-11 14:32:44 -0700

    practice: cpp

---
+++ cpp/temps/tmp4/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::string name = "Joe";
+
+    std::string *pName = &name;
+
+    std::cout << pName;
+
+    return 0;
+}
--- cpp/tmps/tmp2/main.go
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <vendor>
-#include <unordered_map>
-
-std::pair<int, int> run(const std::vector<int>& nums, int target) {
-
-}
