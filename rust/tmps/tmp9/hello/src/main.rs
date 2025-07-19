9d201bc629f03f73d24adcc8e66d24d836a72ab4
Joe Ton <joe@tonindustries.com>
2025-07-18 21:46:41 -0700

    cpp rust

---
+++ rust/tmps/tmp9/hello/src/main.rs
@@ -0,0 +1,58 @@
+use std::collections::HashMap;
+
+
+#[derive(Debug, PartialEq)]
+enum TwoSumError {
+    NoSolution,
+    InvalidCount,
+}
+
+// Find two indices whose values are equal to target
+fn run(nums: &[i32], target: i32) -> Result<(usize, usize), TwoSumError> {
+    // TODO: validate nums and target are true
+    if nums.len() < 2 {
+        return Err(TwoSumError::InvalidCount);
+    }
+
+    let mut seen: HashMap<i32, usize> = HashMap::new();
+
+    for (idx, &num) in nums.iter().enumerate() {
+        let complement = target - num;
+
+        if let Some(&comp_idx) = seen.get(&complement) {
+            return Ok((comp_idx, idx))
+        }
+
+        seen.insert(num, idx);
+    }
+
+    Err(TwoSumError::NoSolution)
+}
+
+
+fn main() {
+    let nums = vec![1, 2, 3, 4];
+    let target = 7;
+
+    match run(&nums, target) {
+        Ok((i, j)) => println!("Result: {i}, {j}"),
+        Err(e) => eprintln!("Error: {:?}", e),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let nums = vec![1, 2, 3, 4];
+        let target = 7;
+
+        match run(&nums, target) {
+            Ok((i, j)) => assert_eq!((i, j), (2, 3)),
+            Err(e) => eprintln!("Error: {:?}", e),
+        }
+
+    }
+}
---
+++ rust/tmps/tmp9/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/joe/projects/ai-daily/rust/tmps/tmp9/hello/target/debug/hello: /home/joe/projects/ai-daily/rust/tmps/tmp9/hello/src/main.rs
