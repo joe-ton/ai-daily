40bf86e31082acedf495e85bb816fac40c1567cc
joe-ton <joeton.public@gmail.com>
2025-08-29 00:48:37 -0700

    practice: rust

---
+++ rust/tmps/tmp2/hello/src/main.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+fn run(nums: vec![i32], target: i32) -> Result<(usize, usize)> {
+    let seen: HashMap<i32, i32> = HashMap::new();
+
+    for idx, num in nums.iter().enumerate() {
+
+    }
+}
+
+
+fn main() {
+    let target = 7;
+    let nums = [1, 2, 3, 4];
+
+    let result = run(nums, target);
+}
