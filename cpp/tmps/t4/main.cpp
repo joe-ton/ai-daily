b256271b1a21c4d996f693c96a8d0518f14e282f
joe-ton <joeton.public@gmail.com>
2025-09-28 11:37:41 -0700

    cpp

---
+++ cpp/tmps/t4/main.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+
+std::vector<int> getResponse(int target, const std::vector<int>& nums) {
+    std::unordered_map<int, int> prev;
+
+    for (int i = 0; i < nums.size(); ++i) {
+        int complement = target - nums[i];
+
+        if (prev.find(complement) != prev.end()) {
+            return {nums[complement], i};
+        }
+
+        prev[nums[i]] = i;
+    }
+
+    return {0, 0};
+}
+
+int main() {
+    int target = 7;
+    std::vector<int> nums = {1, 2, 3, 4};
+
+    auto result = getResponse(target, nums);
+
+    std::cout << "Result: ";
+    for (const auto& idx : result) {
+        std::cout << idx << " ";
+    }
+}
