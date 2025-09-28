6da5780360ccadcb975451fbbe3399da040ab429
joe-ton <joeton.public@gmail.com>
2025-09-27 22:22:17 -0700

    cpp

---
+++ cpp/temps/t10/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+std::vector<int> getResponse(const std::vector<int>& nums, int target) {
+}
+
+int main() {
+    int target = 7;
+    std::vector<int> nums = {1, 2, 3, 4};
+
+    auto result = getResponse(nums, target);
+
+    std::cout << "Result: ";
+    for (const auto& )
+}
