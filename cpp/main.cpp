f0c87388d4adde64af62036b26d240d2121f4cb3
Joe Ton <joe@tonindustries.com>
2026-04-20 07:02:21 -0700

    [ai-daily]

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,31 +1,30 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 
-using namespace std::vector, std::cout, std::endl;
+using std::vector;
 
 vector<int> twoSum(vector<int> nums, int target) {
-    int left = 0, right = nums.size() - 1;
+  int left = 0, right = nums.size() - 1;
 
-    while (left < right) {
-        long long sum = (long long)nums[left] + nums[right];
+  while (left < right) {
+    long long sum = (long long)nums[left] + nums[right];
 
-        if (sum == target) {
-            return {left + 1, right + 1};
-        } else if (sum < target) {
-            left++;
-        } else {
-            right--;
-        }
+    if (sum == target) {
+      return {left + 1, right + 1};
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
     }
-    return {};
+  }
+  return {};
 }
 
 int main() {
-    vector<int> nums = {1, 2, 3, 4};
-    int target = 7;
+  vector<int> nums = {1, 2, 3, 4};
+  int target = 7;
 
-    vector<int> response = twoSum(nums, target);
-    std::cout << "Response: " << response << std::endl();
+  vector<int> response = twoSum(nums, target);
+
+  std::cout << "Response: " << response << std::endl;
 }
-
-main()
