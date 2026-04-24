27d0bbf68779a9ce441b64aa80486118e51cf9c2
Joe Ton <joe@tonindustries.com>
2026-04-23 20:27:24 -0700

    [ai-daily]

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,34 +1,19 @@
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 using std::vector;
 
-std::vector<int> twoPointers(std::vector<int> nums, int target) {
-  int left = 0, right = nums.size() - 1;
+vector<int> twoSum(vector<int> nums, int target) {
+  std::unordered_map<int, int> prev;
 
-  while (left < right) {
-    long long sum = (long long)nums[left] + nums[right];
+  for (const auto &idx : nums) {
+    int complement = target - nums[idx];
 
-    if (sum == target) {
-      return {left + 1, right + 1};
-    } else if (sum < target) {
-      left++;
-    } else {
-      right--;
+    if (prev.find(complement) != prev.end()) {
+      return {prev[complement], idx};
     }
+    prev[nums[idx]] = idx;
   }
   return {};
-}
-
-int main() {
-  vector<int> nums = {1, 2, 3, 4};
-  int target = 7;
-
-  std::vector<int> response = twoPointers(nums, target);
-
-  std::cout << "Response: ";
-  for (const auto &idx : response) {
-    std::cout << idx << " ";
-  }
-  std::cout << std::endl;
 }
